a0bf17870a36458e477f28a97ee63e087ebf5b4e
Gabriel dos Santos Vereda <gabrielsantosvereda@gmail.com>
2026-06-09 15:49:10 -0300

    feat: adiciona estrutura inicial do aplicativo com rotas e estilos

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './styles/App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+import Contato from './pages/Contato'
+import Characters from './pages/Characters'
+import Movies from './pages/Movies'
+import NotFound from './pages/NotFound'
+import './styles/global.css'
+import { useEffect } from 'react'
+
+function App() {
+  useEffect(() => {
+    const ogg = new Audio('/audio/Not_Found.ogg')
+    const mp3 = new Audio('/audio/Not_Found.mp3')
+
+    ogg.preload = 'auto'
+    mp3.preload = 'auto'
+
+    ogg.load()
+    mp3.load()
+  }, [])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sobre' element={<Sobre />} />
+        <Route path='/contato' element={<Contato />} />
+        <Route path='/characters' element={<Characters />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
